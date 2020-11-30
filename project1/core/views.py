33da5cad93e6cfcc5f566f90ee5954ef55ed523a
agdebebe <abulue8@gmail.com>
2020-11-30 15:03:10 -0800

    New Edit

---
+++ project1/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def core(request):
+    return HttpResponse("Main tasks page")
+    #return render(request, '')
