fd4b8b0093cf81a3516c0339b9457953ee249139
agdebebe <abulue8@gmail.com>
2020-11-30 17:20:54 -0800

    checkpoint -3 commit

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 
@@ -120,5 +121,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATICFILES_DIRS = [ STATIC_DIR ]
 STATIC_URL = '/static/'
